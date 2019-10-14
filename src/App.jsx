9087ef21b1e807b874e72f7d3106755003c91fef
kamidev <jonas.beckman@gmail.com>
2019-10-14 20:27:15 +0200

    Support project_id with string values

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
-import Header from './Header';
-import Matrix from './matrix/Matrix';
-import Map from './map/Map';
-import Partners from './partners/Partners'
-import Sidebar from './Sidebar';
-import About from './About';
-import AnimatedInfoBox from './info-box/AnimatedInfoBox';
-import { GetProjectId } from './ProjectNavigator';
-import mockData from './assets/data/mock-data-v8.json';
-import usePreloadImages from './usePreloadImages';
-import './App.css';
-import 'react-widgets/dist/css/react-widgets.css';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
+import Header from "./Header";
+import Matrix from "./matrix/Matrix";
+import Map from "./map/Map";
+import Partners from "./partners/Partners";
+import Sidebar from "./Sidebar";
+import About from "./About";
+import AnimatedInfoBox from "./info-box/AnimatedInfoBox";
+import { GetProjectId } from "./ProjectNavigator";
+import mockData from "./assets/data/mock-data-v10.json";
+import usePreloadImages from "./usePreloadImages";
+import "./App.css";
+import "react-widgets/dist/css/react-widgets.css";
 
 function App() {
   const [filteredData, setFilteredData] = useState(mockData);
@@ -22,15 +27,15 @@ function App() {
 
   const updateFilteredData = newData => {
     setFilteredData(newData);
-  }
+  };
 
   const updateScaleData = newData => {
     setScaleData(newData);
-  }
+  };
 
   const toggleAbout = () => {
     setAbout(!about);
-  }
+  };
 
   return (
     <Router basename={process.env.PUBLIC_URL}>
@@ -38,26 +43,46 @@ function App() {
         <Header toggleAbout={toggleAbout} />
         <div className="App__content">
           <About toggleAbout={toggleAbout} about={about} />
-          <Sidebar data={mockData} updateFilteredData={updateFilteredData} scaleData={scaleData} />
+          <Sidebar
+            data={mockData}
+            updateFilteredData={updateFilteredData}
+            scaleData={scaleData}
+          />
           <Switch>
-            <Route path="/" exact render={props => (
-              <Redirect to="/map" />
-            )}/>
-            <Route path="/map" render={props => (
-              <Map data={mockData} filteredData={filteredData} updateScaleData={updateScaleData} />
-            )}/>
-            <Route path="/matrix" render={props => (
-              <Matrix data={mockData} filteredData={filteredData} updateScaleData={updateScaleData} />
-            )}/>
-            <Route path="/partners" render={props => (
-              <Partners updateScaleData={updateScaleData} />
-            )}/>
+            <Route path="/" exact render={props => <Redirect to="/map" />} />
+            <Route
+              path="/map"
+              render={props => (
+                <Map
+                  data={mockData}
+                  filteredData={filteredData}
+                  updateScaleData={updateScaleData}
+                />
+              )}
+            />
+            <Route
+              path="/matrix"
+              render={props => (
+                <Matrix
+                  data={mockData}
+                  filteredData={filteredData}
+                  updateScaleData={updateScaleData}
+                />
+              )}
+            />
+            <Route
+              path="/partners"
+              render={props => <Partners updateScaleData={updateScaleData} />}
+            />
           </Switch>
-          <Route render={props => (
-            <AnimatedInfoBox
-              data={mockData}
-              id={Number.parseInt(GetProjectId(props.location))}/>
-          )}/>
+          <Route
+            render={props => (
+              <AnimatedInfoBox
+                data={mockData}
+                id={GetProjectId(props.location)}
+              />
+            )}
+          />
         </div>
       </div>
     </Router>
--- src/info-box/AnimatedInfoBox.jsx
+++ src/info-box/AnimatedInfoBox.jsx
@@ -1,35 +1,47 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router';
-import { Transition, animated } from 'react-spring';
-import PropTypes from 'prop-types';
-import InfoBox from './InfoBox';
-import './AnimatedInfoBox.css';
+import React, { Component } from "react";
+import { withRouter } from "react-router";
+import { Transition, animated } from "react-spring";
+import PropTypes from "prop-types";
+import InfoBox from "./InfoBox";
+import "./AnimatedInfoBox.css";
 
 class AnimatedInfoBox extends Component {
   shouldComponentUpdate(nextProps, nextState) {
-    if (nextProps.data !== this.props.data
-      || nextProps.id !== this.props.id
-      || nextProps.location !== this.props.location) return true;
+    if (
+      nextProps.data !== this.props.data ||
+      nextProps.id !== this.props.id ||
+      nextProps.location !== this.props.location
+    )
+      return true;
     return false;
   }
 
   render() {
-    const projectDetails = this.props.data.data
-      .reduce((acc, cur) => ({ ...acc, [cur.survey_answers.project_id]: <InfoBox project={cur} location={this.props.location} /> }), {});
+    const projectDetails = this.props.data.data.reduce(
+      (acc, cur) => ({
+        ...acc,
+        [cur.survey_answers.project_id]: (
+          <InfoBox project={cur} location={this.props.location} />
+        )
+      }),
+      {}
+    );
 
     return (
       <Transition
         native
         items={this.props.id}
-        from={{ opacity: 0, transform: 'translateX(80%)' }}
-        enter={{ opacity: 1, transform: 'translateX(0)' }}
-        leave={{ opacity: 0, transform: 'translateX(70%)'}}>
+        from={{ opacity: 0, transform: "translateX(80%)" }}
+        enter={{ opacity: 1, transform: "translateX(0)" }}
+        leave={{ opacity: 0, transform: "translateX(70%)" }}
+      >
         {id =>
           id !== -1 &&
-            (props => 
-              <animated.div className="info-box-container" style={{ ...props}}>
-                {projectDetails[id]}
-              </animated.div>)
+          (props => (
+            <animated.div className="info-box-container" style={{ ...props }}>
+              {projectDetails[id]}
+            </animated.div>
+          ))
         }
       </Transition>
     );
@@ -38,7 +50,7 @@ class AnimatedInfoBox extends Component {
 
 AnimatedInfoBox.propTypes = {
   data: PropTypes.object,
-  id: PropTypes.number
-}
+  id: PropTypes.any
+};
 
 export default withRouter(AnimatedInfoBox);
